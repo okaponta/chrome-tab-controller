6c37c1d5ef0f5d2dee522284493742849c241063
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-08 15:10:09 +0900

    enabled to move the tab with shortcut
    Co-authored-by: Yuki Fujita <huji0327yuu@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,23 +1,26 @@
-chrome.tabs.onActivated.addListener(() => move());
+chrome.commands.onCommand.addListener(async (command) => {
+  console.log(command);
+  move();
+});
 
 async function move() {
-    console.log('test');
-    let curTab = await getCurrentTab();
-    console.log(curTab);
-    console.log(curTab.id);
+  let curTab = await getCurrentTab();
 
-    try {
-        await chrome.tabs.move(curTab.id, { index: 0 });
-        console.log('Success.');
-    } catch (error) {
-        if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
-            setTimeout(() => move(), 50);
-        }
+  try {
+    await chrome.tabs.move(curTab.id, { index: 0 });
+    console.log("Success.");
+  } catch (error) {
+    if (
+      error ==
+      "Error: Tabs cannot be edited right now (user may be dragging a tab)."
+    ) {
+      setTimeout(() => move(), 50);
     }
+  }
 }
 
 async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
